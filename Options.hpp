cfcc2c0900e2ded9ae8361392dd9545a75de1803
killcerr <93478098+killcerr@users.noreply.github.com>
2024-04-30 21:13:39 +0800

    Add files via upload

--- Options.hpp
+++ Options.hpp
@@ -3,21 +3,52 @@
 
 
 extern "C" std::size_t std::strlen(const char*);
-extern "C" int         strcmp(const char*, const char*);
+extern "C" int         std::strcmp(const char*, const char*);
+
+#define EXPECT(COND, ...)                                                                                              \
+    [&] {                                                                                                              \
+        if (COND) try {                                                                                                \
+                return Expection{__VA_ARGS__};                                                                         \
+            } catch (...) {}                                                                                           \
+        return Expection<decltype(__VA_ARGS__)>{};                                                                     \
+    }()
+
 namespace opt_helper {
+
+template <typename T>
+struct Expection {
+    T* value                                         = nullptr;
+    constexpr Expection()                            = default;
+    constexpr Expection(const Expection&)            = default;
+    constexpr Expection(Expection&&)                 = default;
+    constexpr Expection& operator=(const Expection&) = default;
+    constexpr Expection& operator=(Expection&&)      = default;
+    constexpr Expection(T& d) { value = &d; }
+    constexpr Expection(T d) { value = &d; }
+    constexpr T& expect(auto&& f) {
+        if (value == nullptr) f();
+        return *value;
+    }
+    constexpr const T& expect(auto&& f) const {
+        if (value == nullptr) f();
+        return *value;
+    }
+};
+
+
 template <typename T>
 struct OptVal {
     T    data;
-    auto toi() { return std::stoi(data); }
-    auto tol() { return std::stol(data); }
-    auto toll() { return std::stoll(data); }
-    auto toul() { return std::stoul(data); }
-    auto toull() { return std::stoull(data); }
-    auto tod() { return std::stod(data); }
-    auto tof() { return std::stof(data); }
-    auto told() { return std::stold(data); }
-    auto tob() { return std::strcmp(data, "true") == 0; }
-    auto top() { return std::filesystem::path(data); }
+    auto toi() { return EXPECT(data != nullptr, std::stoi(data)); }
+    auto tol() { return EXPECT(data != nullptr, std::stol(data)); }
+    auto toll() { return EXPECT(data != nullptr, std::stoll(data)); }
+    auto toul() { return EXPECT(data != nullptr, std::stoul(data)); }
+    auto toull() { return EXPECT(data != nullptr, std::stoull(data)); }
+    auto tod() { return EXPECT(data != nullptr, std::stod(data)); }
+    auto tof() { return EXPECT(data != nullptr, std::stof(data)); }
+    auto told() { return EXPECT(data != nullptr, std::stold(data)); }
+    auto tob() { return EXPECT(data != nullptr, std::strcmp(data, "true") == 0); }
+    auto top() { return EXPECT(data != nullptr, std::filesystem::path(data)); }
     auto empty() { return data == nullptr || data[0] == '\0'; }
 };
 
