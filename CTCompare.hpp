58d38ab2ba1f564f4f1dd09d5260a324c5d85dd1
killcerr <93478098+killcerr@users.noreply.github.com>
2024-07-16 21:15:30 +0800

    feat: improve operator==

--- CTCompare.hpp
+++ CTCompare.hpp
@@ -4,17 +4,22 @@
 
 namespace CTC {
 template <details::CTCC T, details::CTCC U>
-[[nodiscard]] constexpr std::weak_ordering operator<=>(const T& a, const U& b) {
+[[nodiscard]] constexpr std::strong_ordering operator<=>(const T& a, const U& b) {
     auto ai = a.cbegin();
     auto bi = b.cbegin();
     for (; ai != a.cend() && bi != b.cend() && *ai == *bi; ai++, bi++)
         ;
-    if (ai == a.cend() && bi == b.cend()) return std::weak_ordering::equivalent;
-    if (*ai > *bi) return std::weak_ordering::greater;
-    else return std::weak_ordering::less;
+    if (ai == a.cend() && bi == b.cend()) return std::strong_ordering::equivalent;
+    if (*ai > *bi) return std::strong_ordering::greater;
+    else return std::strong_ordering::less;
 }
 template <details::CTCC T, details::CTCC U>
 [[nodiscard]] constexpr bool operator==(const T& a, const U& b) {
-    return a <= b && b <= a;
+    auto ai = a.cbegin();
+    auto bi = b.cbegin();
+    for (; ai != a.cend() && bi != b.cend() && *ai == *bi; ai++, bi++)
+        ;
+    if (ai == a.cend() && bi == b.cend()) return true;
+    else return false;
 }
 } // namespace CTC
