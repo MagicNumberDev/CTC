55e1665bf20faa0156a7b1b73ec3ea21e93176ec
killcerr <93478098+killcerr@users.noreply.github.com>
2024-08-10 22:27:00 +0800

    feat: value_type_of

--- CTCBase.hpp
+++ CTCBase.hpp
@@ -22,6 +22,8 @@ template <auto I, typename T>
 struct TypeContainerBase<I, T> {
     static auto get(Index<I>) { return Type<T>{}; }
 };
+template <typename T>
+concept HasValueType = requires { typename T::value_type; };
 } // namespace details
 template <typename... Ts>
 struct TypeContainer : details::TypeContainerBase<0, Ts...> {
@@ -29,4 +31,12 @@ struct TypeContainer : details::TypeContainerBase<0, Ts...> {
     template <auto I>
     using type = decltype(get(details::Index<I>{}))::type;
 };
+template <typename T>
+struct value_type_of;
+template <details::HasValueType T>
+struct value_type_of<T> {
+    using type = typename T::value_type;
+};
+template <typename T>
+using value_type_of_v = typename value_type_of<T>::type;
 } // namespace CTC
