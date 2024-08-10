732ff7b88eda690a8dd062cf0b2d85e93c49ef6e
killcerr <93478098+killcerr@users.noreply.github.com>
2024-08-10 22:18:56 +0800

    feat: TypeContainer

--- CTCBase.hpp
+++ CTCBase.hpp
@@ -5,5 +5,28 @@ struct basic_CTC_container {};
 namespace details {
 template <typename T>
 concept CTCC = std::is_base_of_v<basic_CTC_container, T>;
-}
+template <auto I>
+struct Index {};
+template <typename T>
+struct Type {
+    using type = T;
+};
+template <auto I, typename... Ts>
+struct TypeContainerBase;
+template <auto I, typename T, typename... Ts>
+struct TypeContainerBase<I, T, Ts...> : TypeContainerBase<I + 1, Ts...> {
+    using TypeContainerBase<I + 1, Ts...>::get;
+    static auto get(Index<I>) { return Type<T>{}; }
+};
+template <auto I, typename T>
+struct TypeContainerBase<I, T> {
+    static auto get(Index<I>) { return Type<T>{}; }
+};
+} // namespace details
+template <typename... Ts>
+struct TypeContainer : details::TypeContainerBase<0, Ts...> {
+    using details::TypeContainerBase<0, Ts...>::get;
+    template <auto I>
+    using type = decltype(get(details::Index<I>{}))::type;
+};
 } // namespace CTC
