1a5070c20aa0e7bf8fbb81120737b20ed190dc2b
killcerr <93478098+killcerr@users.noreply.github.com>
2024-08-10 23:25:37 +0800

    fix: fix deduction guide

--- Function.hpp
+++ Function.hpp
@@ -20,6 +20,22 @@ template <typename RetType, typename ObjType, typename... ArgsType>
 struct GetFunctionType<RetType (ObjType::*)(ArgsType...)> {
     using type = RetType(ArgsType...);
 };
+template <typename RetType, typename ObjType>
+struct GetFunctionType<RetType (ObjType::*)() const> {
+    using type = RetType();
+};
+template <typename RetType, typename ObjType>
+struct GetFunctionType<RetType (ObjType::*)() volatile> {
+    using type = RetType();
+};
+template <typename RetType, typename ObjType>
+struct GetFunctionType<RetType (ObjType::*)() const volatile> {
+    using type = RetType();
+};
+template <typename RetType, typename ObjType>
+struct GetFunctionType<RetType (ObjType::*)()> {
+    using type = RetType();
+};
 } // namespace details
 template <typename T>
 struct Function;
@@ -32,9 +48,23 @@ struct Function<RetType(ArgsType...)> {
         call = [](Any& fn, ArgsType... args) { return (fn.cast_to_ref<T>())(args...); };
     }
     auto operator()(ArgsType&&... args) { return call(fn, args...); }
+    Function(std::nullptr_t) : fn(nullptr) {}
+};
+template <typename RetType>
+struct Function<RetType()> {
+    Any fn;
+    RetType (*call)(Any&);
+    template <typename T>
+    Function(T f) : fn(f) {
+        call = [](Any& fn) { return (fn.cast_to_ref<T>())(); };
+    }
+    auto operator()() { return call(fn); }
+    Function(std::nullptr_t) : fn(nullptr) {}
 };
 template <typename T>
 Function(T) -> Function<typename details::GetFunctionType<decltype(&T::operator())>::type>;
 template <typename RetType, typename... ArgsType>
 Function(RetType (*)(ArgsType...)) -> Function<RetType(ArgsType...)>;
+template <typename RetType>
+Function(RetType (*)()) -> Function<RetType()>;
 } // namespace CTC
