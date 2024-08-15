e284adece77729a3a92b1bb357623f34f1250ee9
killcerr <93478098+killcerr@users.noreply.github.com>
2024-08-15 23:24:12 +0800

    feat: rename

--- test/ObjectTest.cpp
+++ test/ObjectTest.cpp
@@ -46,6 +46,7 @@ auto& test(auto v) {
 
 
 int main() {
+    // testing::ScopeTimer mainTimer{"main"};
     X x{};
     printf("%p\n", std::addressof(x));
     AnyObject::Object obj(std::move(x));
@@ -58,24 +59,26 @@ int main() {
     printf("%p,%p,%p\n", b.cast<X>(), c.cast<X>(), t.cast<X>());
     printf("%p\n", std::addressof(test(b.ref())));
     printf("end\n");
-    AnyObject::Object::register_predef_rules();
+    AnyObject::Object::register_pre_def_rules();
     AnyObject::Object e{std::string("1000")};
     auto              r = e.type_cast<char*>();
     std::println("{}", *r.cast<char*>());
     std::vector<AnyObject::Object> a;
     AnyObject::Object              test1(1);
     a.push_back(std::move(test1));
-    AnyObject::CallableObject callobject([](const int& a) {
+    AnyObject::CallableObject callableObject([](const int& a) {
         printf("%d\n", a);
         return std::string("123");
     });
-    auto                      res = callobject.call(a);
+    auto                      res = callableObject.call(a);
     printf("%s\n", res.cast<std::string>()->c_str());
-    callobject.call(*AnyObject::CallableObject::make_args(1));
+    callableObject.call(*AnyObject::CallableObject::make_args(1));
     std::string                    s{};
     std::vector<AnyObject::Object> args;
     while (std::cin >> s) {
         args.emplace_back(s);
     }
-    callobject.call(args);
+    callableObject.call(args);
+    AnyObject::Object n1;
+    n1 = AnyObject::Object{X{}};
 }
